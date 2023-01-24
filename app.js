95b893087ed7af69d47e220f42318a50f3cef4fe
Rahmat Fajar <rahmatfajar3218@gmail.com>
2023-01-25 02:11:32 +0700

    new

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const pool = new Pool({
 
 var indexRouter = require('./routes/index')(pool);
 var usersRouter = require('./routes/users')(pool);
+var unitsRouter = require('./routes/units')(pool);
 
 var app = express();
 
@@ -39,6 +40,7 @@ app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/units', unitsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/units.js
@@ -0,0 +1,16 @@
+var express = require('express');
+const { isLoggedIn } = require('../helpers/utils');
+var router = express.Router();
+
+module.exports = function (db) {
+
+    router.get('/', async function (req, res){
+        res.render('users/users', {
+            currentPage: 'user',
+            user: req.session.user,
+            rows
+        })
+    })
+
+    return router;
+}
