82c2d4d2070c8da63a2597fa508bea9eaf7bbca3
Rahmat Fajar <rahmatfajar3218@gmail.com>
2023-02-12 23:58:49 +0700

    next

--- routes/goods.js
+++ routes/goods.js
@@ -80,32 +80,35 @@ module.exports = function (db) {
             })
 
         }
+
+
+        
         catch (e) {
             res.send(e)
         }
     });
 //upload file
-    router.post('/upload', function(req, res) {
-        let fileName = req.body,fileNamefileName;
-        let sampleFile;
-        let uploadPath;
+    // router.post('/upload', function(req, res) {
+    //     let fileName = req.body,fileNamefileName;
+    //     let sampleFile;
+    //     let uploadPath;
       
-        if (!req.files || Object.keys(req.files).length === 0) {
-          return res.status(400).send('No files were uploaded.');
-        }
+    //     if (!req.files || Object.keys(req.files).length === 0) {
+    //       return res.status(400).send('No files were uploaded.');
+    //     }
       
-        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-        sampleFile = req.files.sampleFile;
-        uploadPath = path.join( __dirname,'..','views','goods','upload', `${Date.now()}-${sampleFile.name}`);
+    //     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    //     sampleFile = req.files.sampleFile;
+    //     uploadPath = path.join( __dirname,'..','views','goods','upload', `${Date.now()}-${sampleFile.name}`);
       
-        // Use the mv() method to place the file somewhere on your server
-        sampleFile.mv(uploadPath, function(err) {
-          if (err)
-            return res.status(500).send(err);
+    //     // Use the mv() method to place the file somewhere on your server
+    //     sampleFile.mv(uploadPath, function(err) {
+    //       if (err)
+    //         return res.status(500).send(err);
       
-          res.send('File uploaded!');
-        });
-      });
+    //       res.send('File uploaded!');
+    //     });
+    //   });
 
 
 
