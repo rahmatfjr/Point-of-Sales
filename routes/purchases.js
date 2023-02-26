237da8ca16b93558aeef680b48fca2b8de6981c1
Rahmat Fajar <rahmatfajar3218@gmail.com>
2023-02-26 23:10:14 +0700

    jquery err

--- routes/purchases.js
+++ routes/purchases.js
@@ -64,16 +64,16 @@ module.exports = function (db) {
     })
 
     router.get('/show/:invoice', isLoggedIn, async function (req, res) {
-   
+
         try {
             const getpurchases = await db.query('SELECT purchases.*, suppliers.* FROM purchases LEFT JOIN suppliers ON purchases.operator = suppliers.supplierid WHERE invoice = $1', [req.params.invoice])
-            const { rows: suppliers } = await db.query('SELECT * FROM suppliers ORDER BY supplierid')
+            const getsupplier = await db.query('SELECT * FROM suppliers ORDER BY supplierid')
             const { rows: goods } = await db.query('SELECT * FROM goods ORDER BY barcode')
             res.render('purchases/add', {
                 user: req.session.user,
                 currentPage: 'add',
                 purchases: getpurchases.rows[0],
-                suppliers: suppliers.rows,
+                suppliers: getsupplier.rows,
                 goods
             })
             console.log(getpurchases.rows[0], 'harus ada')
@@ -84,7 +84,73 @@ module.exports = function (db) {
 
     });
 
+    router.get('/details/:invoice', isLoggedIn, async function (req, res) {
+        const invoice = req.params.invoice
+        try {
+            const { rows } = await db.query(`SELECT purchaseitems.* , goods.name FROM purchaseitems LEFT JOIN goods ON purchaseitems.itemcode = goods.barcode WHERE purchaseitems.invoice = $1 ORDER BY purchaseitems.id`, [invoice])
+            res.json(rows)
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    });
 
+
+    router.get('/goods/:barcode', isLoggedIn, async function (req, res) {
+        const barcode = req.params.barcode
+        try {
+            const { rows } = await db.query(`SELECT * FROM goods WHERE barcode = $1`, [barcode])
+            res.json(rows[0])
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    });
+
+    router.get('/deleteitems/:id', isLoggedIn, async function (req, res) {
+        const id = req.params.id
+        try {
+            const { rows } = await db.query(`DELETE FROM purchaseitems WHERE id = $1 returning *`, [id])
+            res.redirect(`/purchases/show/${rows[0].invoice}`)
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    });
+
+
+    router.post('/show/:invoice', isLoggedIn, async function (req, res) {
+        const invoice = req.params.invoice
+        const { totalsummary, supplier } = req.body
+        const userid = req.session.user.userid
+        try {
+
+            if (!supplier) {
+                await db.query('UPDATE purchases SET totalsum = $1, operator = $2 WHERE invoice = $3', [totalsummary, userid, invoice])
+            } else {
+                await db.query('UPDATE purchases SET totalsum = $1, supplier = $2, operator = $3 WHERE invoice = $4', [totalsummary, supplier, userid, invoice])
+            }
+
+            res.redirect(`/purchases`)
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    });
+
+    router.post('/additem', isLoggedIn, async function (req, res) {
+        const { invoice, barcode, quantity } = req.body
+
+        try {
+            await db.query(`INSERT INTO purchaseitems (invoice, itemcode, quantity) VALUES($1, $2, $3) returning *`, [invoice,barcode, quantity])
+            const { rows } = await db.query('SELECT * FROM purchases WHERE invoice = $1', [invoice])
+            res.json(rows[0])
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    });
+    
 
     return router;
 }
--- triggerpurchase.sql
+++ triggerpurchase.sql
@@ -1,44 +1,49 @@
-CREATE OR REPLACE FUNCTION update_purchase() RETURNS TRIGGER AS $set_purchase$
+    CREATE OR REPLACE FUNCTION update_purchases() RETURNS TRIGGER AS $set_purchases$
     DECLARE
     stock_lama INTEGER;
     sum_harga NUMERIC;
+    current_invoice TEXT;
     BEGIN
         IF (TG_OP = 'INSERT') THEN
-            SELECT stock INTO stock_lama FROM goods WHERE barcode = NEW.barcode;
-            UPDATE goods SET stock = stock_lama + NEW.quantity WHERE barcode = NEW.barcode;
-            
-        ELSIF (TG_OP = 'UPDATE') THEN
-            SELECT stock INTO stock_lama FROM goods WHERE barcode = NEW.barcode;
-            UPDATE goods SET stock = stock_lama + OLD.qty - NEW.quantity WHERE barcode = NEW.barcode;
-
-        ELSIF (TG_OP = 'DELETE') THEN
-            SELECT stock INTO stock_lama FROM goods WHERE barcode = NEW.barcode;
-            UPDATE goods SET stock = stock_lama - NEW.quantity WHERE barcode = NEW.barcode;
+            SELECT stock INTO stock_lama FROM goods WHERE barcode = NEW.itemcode;
+            UPDATE goods SET stock = stock_lama + NEW.quantity WHERE barcode = NEW.itemcode;
+            current_invoice := NEW.invoice;
         
-        END IF;
-         SELECT sum(totalprice) INTO sum_harga FROM purchaseitems WHERE invoice = NEW.invoice;
-        UPDATE purchase SET totalprice = sum_harga WHERE purchase.invoice = NEW.invoice;
-        RETURN NULL;
-    END;
-$set_purchase$ LANGUAGE plpgsql;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            SELECT stock INTO stock_lama FROM goods WHERE barcode = NEW.itemcode;
+            UPDATE goods SET stock = stock_lama - OLD.quantity + NEW.quantity WHERE barcode = NEW.itemcode;
+            current_invoice := NEW.invoice;
+        
+        ELSIF (TG_OP = 'DELETE') THEN
+            SELECT stock INTO stock_lama FROM goods WHERE barcode = OLD.itemcode;
+            UPDATE goods SET stock = stock_lama - OLD.quantity WHERE barcode = OLD.itemcode;
+            current_invoice := OLD.invoice;
 
-CREATE TRIGGER set_purchase
+        END IF;
+        SELECT sum(totalprice) INTO sum_harga FROM purchaseitems WHERE invoice = current_invoice;
+        UPDATE purchases SET totalsum = sum_harga WHERE invoice = current_invoice;
+
+        RETURN NULL; -- result is ignored since this is an AFTER trigger
+    END;
+$set_purchases$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_purchases
 AFTER INSERT OR UPDATE OR DELETE ON purchaseitems
-    FOR EACH ROW EXECUTE FUNCTION update_purchase();
+    FOR EACH ROW EXECUTE FUNCTION update_purchases();
 
 
 --update total harga
-CREATE OR REPLACE FUNCTION purchaseitems() RETURNS TRIGGER AS $set_purchaseitems$
-    DECLARE
-    purchaseprice_items NUMERIC;
+CREATE OR REPLACE FUNCTION update_price() RETURNS TRIGGER AS $set_total_price$
+    DECLARE 
+    total_price_purchase NUMERIC;
     BEGIN
-        SELECT purchaseprice INTO purchaseprice_items FROM barang WHERE barcode = NEW.barcode;
-        NEW.purchaseprice := purchaseprice_items;
-        NEW.total_harga := NEW.qty * purchaseprice_items;
+        SELECT purchaseprice INTO total_price_purchase FROM goods WHERE barcode = NEW.itemcode;
+        NEW.purchaseprice := total_price_purchase;
+        NEW.totalprice := NEW.quantity * NEW.purchaseprice;
         RETURN NEW;
     END;
-$set_purchaseitems$ LANGUAGE plpgsql;
+$set_total_price$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_purchaseitems
+CREATE TRIGGER set_total_price
 BEFORE INSERT OR UPDATE ON purchaseitems
-    FOR EACH ROW EXECUTE FUNCTION purchaseitems();
+    FOR EACH ROW EXECUTE FUNCTION update_price();
